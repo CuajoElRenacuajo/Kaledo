ae3f73e111f7bb0272f8d7471707f3c90d09cd86
Saúl Napoleón Haro Hernández <93763098+CuajoElRenacuajo@users.noreply.github.com>
2024-01-16 20:18:58 -0600

    Pipeline Test

---
+++ Assets/Code/player.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    protected void Update()
+    {
+        Debug.Log(CompareTag("tag1"));
+    }
+}
